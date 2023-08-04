d8fa30ddd4a71a964d2343820aba699256de4606
Kosta Gjorgjievski <kostagj@g.ucla.edu>
2023-08-04 16:34:36 -0700

    - Added Font Awesome to the Website - Set up the base template of the website, connected vue and django - Created a django app and models for products (product, category)

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'djoser',
 
+    'product'
+
 ]
 
 #change this to the live server address when you deploy
@@ -128,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- backend_django/backend_django/urls.py
+++ backend_django/backend_django/urls.py
@@ -1,11 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+fom django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/vi/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
-]
+] + static(settigns.MEDIA_URL, document_root=settigns.MEDIA_ROOT)
 
 
