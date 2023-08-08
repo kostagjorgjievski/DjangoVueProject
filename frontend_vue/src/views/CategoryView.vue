7af7cbbfc21aab8931cb3385e5d3628403c16185
Kosta Gjorgjievski <kostagj@g.ucla.edu>
2023-08-07 23:11:07 -0700

    Added compoment ProductBox, fixed routing through watch

--- frontend_vue/src/views/CategoryView.vue
+++ frontend_vue/src/views/CategoryView.vue
@@ -5,20 +5,12 @@
                 <h2 class="is-size-2 has-text-centered"> {{ category.name }} </h2>
             </div>
 
-            <div 
-                v-for="product in category.products"
-                v-bind:key="product.id"
-                >
-                <div class="box">
-                <figure class="image mb-4">
-                    <img v-bind:src="product.get_thumbnail">
-                </figure>
-                <h3 class="is-size-4">{{ product.name }}</h3>
-                <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+            
+      <ProductBox
+        v-for="product in category.products"
+        v-bind:key="product.id"
+        v-bind:product="product" />
 
-                <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View Details</router-link>
-                </div>
-            </div>
         </div>
     </div>
 </template>
@@ -27,6 +19,7 @@
 
 import axios from 'axios'
 import { toast } from 'bulma-toast'
+import ProductBox from '@/components/ProductBox.vue'
 
 export default {
     name: 'Category',
@@ -37,8 +30,18 @@ export default {
             }
         }
     },
+    components: {
+        ProductBox
+    },
     mounted() {
         this.getCategory()
+    },
+    watch: {
+        $route(to, from) {
+            if (to.name === 'Category'){
+                this.getCategory()
+            }
+        }
     },
     methods: {
         async getCategory(){
--- frontend_vue/src/views/HomeView.vue
+++ frontend_vue/src/views/HomeView.vue
@@ -16,26 +16,19 @@
           <h2 class="is-size-2 has-text-centered">Latest products</h2>
       </div>
 
-      <div 
+      <ProductBox
         v-for="product in latestProducts"
         v-bind:key="product.id"
-        >
-        <div class="box">
-          <figure class="image mb-4">
-            <img v-bind:src="product.get_thumbnail">
-          </figure>
-          <h3 class="is-size-4">{{ product.name }}</h3>
-          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+        v-bind:product="product" />
 
-          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View Details</router-link>
-        </div>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+
+import ProductBox from '@/components/ProductBox.vue'
 
 
 // @ is an alias to /src
@@ -48,6 +41,7 @@ export default {
     }
   },
   components: {
+    ProductBox
   },
   mounted(){
     this.getLatestProducts()
@@ -66,10 +60,3 @@ export default {
 }
 </script>
 
-<style scoped>
-  .image {
-    margin-top: -1.25rem;
-    margin-left: -1.25rem;
-    margin-right: 1.25rem;
-  }
-</style>
